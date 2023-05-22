468b2d24c3aae7f005d9a91ae68eacf6a3860f16
JorgeTarancon <jorge.tarancon@salud.madrid.org>
2023-05-22 13:45:30 +0200

    Register Ruta model into admin.py file

--- FARMAHOME/admin.py
+++ FARMAHOME/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import DatoReparto
+from .models import DatoReparto, Ruta
 
 # Register your models here.
 admin.site.register(DatoReparto)
+admin.site.register(Ruta)
